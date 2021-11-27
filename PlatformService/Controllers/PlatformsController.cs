b969ec219a7296c19ff59b0fdc1904906db4e267
Ivan Pivovarov <pivovarov@mindbox.ru>
2021-11-27 13:41:35 +0300

    feat: Создал базовый проект, написал базовое rest api

---
+++ PlatformService/Controllers/PlatformsController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PlatformService.Data;
+using PlatformService.Dtos;
+using PlatformService.Models;
+
+namespace PlatformService.Controllers
+{
+    [Route("api/platforms")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        private readonly IPlatformRepo _repository;
+
+        private readonly IMapper _mapper;
+        
+        public PlatformsController(IPlatformRepo repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+        {
+            var allPlatforms = _repository.GetAllPlatforms();
+            
+            return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(allPlatforms));
+        }
+
+        [HttpGet("{id}", Name = "GetPlatformById")]
+        public ActionResult<IEnumerable<PlatformReadDto>> GetPlatformById(int id)
+        {
+            var item = _repository.GetPlatformById(id);
+
+            if (item != null)
+                return Ok(_mapper.Map<PlatformReadDto>(item));
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        public ActionResult<PlatformReadDto> CreatePlatform(PlatformCreateDto platformCreateDto)
+        {
+            var platformModel = _mapper.Map<Platform>(platformCreateDto);
+            
+            _repository.CreatePlatform(platformModel);
+            _repository.SaveChanges();
+
+            var platformReadDto = _mapper.Map<PlatformReadDto>(platformModel);
+
+            return CreatedAtRoute(nameof(GetPlatformById), new {Id = platformModel.Id}, platformReadDto);
+        }
+    }
+}
---
+++ PlatformService/Data/PrepDb.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using PlatformService.Models;
+
+namespace PlatformService.Data
+{
+    public static class PrepDb
+    {
+        private const int CountData = 10;
+
+        private static readonly string[] CostData = {"Free", "1", "10", "100", "1000"};
+        private static readonly string[] PublisherData = {"Microsoft", "Oracle", "Intel", "AMD", "IBM"};
+
+
+        public static void PrepPopulation(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>());
+            }
+        }
+
+        private static void SeedData(AppDbContext context)
+        {
+            if (!context.Platforms.Any())
+            {
+                Console.WriteLine("Заполняем БД тестовыми данными");
+
+                var random = new Random();
+
+                for (var i = 0; i < CountData; i++)
+                {
+                    var name = "";
+                    for (var j = 0; j < 10; j++)
+                        name += (char) random.Next('A', 'Z' + 1);
+                    
+                    context.Platforms.Add(new Platform()
+                    {
+                        Name = name,
+                        Publisher = PublisherData[random.Next(PublisherData.Length)],
+                        Cost = CostData[random.Next(CostData.Length)],
+                    });
+                }
+
+                context.SaveChanges();
+            }
+            else
+            {
+                Console.WriteLine("В БД есть тестовые данные");
+            }
+        }
+    }
+}
